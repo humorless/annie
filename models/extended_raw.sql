f1bbbbc1476e4d2fcc2dfbfa96b5947aab3721b7
Laurence Chen <humorless@gmail.com>
2022-03-24 00:27:25 +0800

    add models/schema.yml to experiment dbt test cmd

--- models/extended_raw.sql
+++ models/extended_raw.sql
@@ -5,10 +5,10 @@ pal as (
   select * from {{ ref('primary_account_list') }}
 )
 select
-  "Data Source",
-  "Sales Team Name",
-  "Campaign Name",
-  "Campaign ID",
+  "Data Source" as data_source,
+  "Sales Team Name" as sales_team_name,
+  "Campaign Name" as campaign_name,
+  "Campaign ID" as campaign_id,
   "Advertiser Customer ID_new" as advertiser_customer_id,
   "Advertiser TAX ID_new" as advertiser_tax_id,
   "Advertiser Name_new" as advertiser_name
