ffad37b9baa3b5ef3dffba3b142fca1d249ff389
Laurence Chen <humorless@gmail.com>
2022-03-23 16:35:12 +0800

    remove full space character

--- models/primary_account_list.sql
+++ models/primary_account_list.sql
@@ -1,7 +1,7 @@
 select distinct
        "Campaign ID_new", 
        "Advertiser Customer ID_new",
-       "Advertiser TAX ID＿new"
+       "Advertiser TAX ID_new",
        "Advertiser Name_new"
 from {{ ref('account_list') }}
 
